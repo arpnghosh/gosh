24e0f0bd4fa7f0565ed5cffc0ff43b9895b125a3
Arpan Ghosh <arpnghosh@protonmail.com>
2024-08-09 22:24:53 +0530

    add: more built-in commands

--- cmd/src/main.go
+++ cmd/src/main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
+
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		input, err := reader.ReadString('\n')
@@ -16,16 +17,39 @@ func main() {
 			fmt.Fprint(os.Stderr, "Error reading input:", err)
 		}
 		input = strings.TrimSpace(input)
+
 		if input == "exit" {
 			os.Exit(0)
 		}
 		args := strings.Fields(input)
+
+		if len(args) == 0 {
+			continue
+		}
 		command := args[0]
 		switch command {
+
 		case "echo":
 			fmt.Println(strings.Join(args[1:], " "))
+
+		case "pwd":
+			fmt.Println(os.Getwd())
+
+		case "ls":
+			files, _ := os.ReadDir(".")
+			for _, file := range files {
+				fmt.Println(file.Name())
+			}
+
+		case "cd":
+			// cd    --> /home/
+			// cd .. --> previous directory
+			// err   --> when cd into a file, show err not a directory
+			dir := strings.Join(args[1:], " ")
+			os.Chdir(dir)
+
 		default:
-			fmt.Println(input + ": command not found")
+			fmt.Println("gosh: command not found: " + input)
 
 		}
 	}
